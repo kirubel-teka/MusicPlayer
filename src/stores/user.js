6cf14cf33acad591b01170c44e607c8584347c0a
Kirubel <se.kirubel.teka@gmail.com>
2024-04-23 04:43:26 -0700

    update header login/logout

--- src/stores/user.js
+++ src/stores/user.js
@@ -29,5 +29,10 @@ export default defineStore('user', {
 
        this.userLoggedIn = true;
       },
+      async signOut() {
+        await auth.signOut();
+
+        this.userLoggedIn = false;
+      }
     },
 });
