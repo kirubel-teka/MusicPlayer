e9f807f6d48fbb3ef304b9529c26e7f0a77e001c
Kirubel <se.kirubel.teka@gmail.com>
2024-04-25 22:18:47 +0300

    Redirect upon logout

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -22,7 +22,7 @@
             <router-link class="px-2 text-white" :to="{ name: 'manage' }">Manage</router-link>
           </li>
           <li>
-            <a class="px-2 text-white" href="#" @click.prevent="userStore.signOut">Logout</a>
+            <a class="px-2 text-white" href="#" @click.prevent="signOut">Logout</a>
           </li>
           </template>
           
@@ -46,6 +46,13 @@ export default {
     toggleAuthModal() {
       this.modalStore.isOpen = !this.modalStore.isOpen
       console.log(this.modalStore.isOpen)
+    },
+    signOut() {
+      this.userStore.signOut();
+      if(this.$route.name === 'manage') {
+        this.$router.push({ name: 'home' })
+      }
+      
     }
   }
 }
