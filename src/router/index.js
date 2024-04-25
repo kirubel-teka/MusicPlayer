920f35c37c3e078ab8f52d16eb7628844b0245b5
Kirubel <se.kirubel.teka@gmail.com>
2024-04-25 21:22:01 +0300

    Header active links and route name

--- src/router/index.js
+++ src/router/index.js
@@ -3,24 +3,38 @@ import Home from "@/views/HomeView.vue"
 import About from "@/views/AboutView.vue"
 import Manage from "@/views/ManageView.vue"
 
+
 const routes = [
   {
+    name: 'home',
     path: "/",
     component: Home,
   },
   {
+    name: 'about',
     path: "/about",
     component: About,
   },
 {
-    path: "/manage",
+    name: 'manage',
+    path: "/manage-music",
     component: Manage
+},
+{
+  path: '/manage',
+  redirect: { name: 'manage'},
+},
+
+{
+  path: '/:catchAll(.*)*',
+  redirect : { name: 'home'},
 }
 ];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
+  linkExactActiveClass: 'text-yellow-500'
 })
 
 export default router
