9236b7d1fb3a01048f7920df170772f8b0f3f8ec
Kirubel <se.kirubel.teka@gmail.com>
2024-04-30 19:32:52 +0300

    file input

--- src/components/AppUpload.vue
+++ src/components/AppUpload.vue
@@ -21,6 +21,7 @@
                 >
                 <h5>Drop your files here</h5>
               </div>
+              <input type="file" multiple @change="upload($event)"/>
               <hr class="my-6" />
               <!-- Progess Bars -->
               <div class="mb-4" v-for="upload in uploads" :key="upload.name">
@@ -53,8 +54,10 @@ export default {
     methods: {
         upload($event) {
            this.is_dragover = false;
+           console.log($event);
            
-           const files = [...$event.dataTransfer.files];
+           const files = $event.dataTransfer ? [...$event.dataTransfer.files] :
+           [...$event.target.files];
 
            files.forEach((file) => {
             if (file.type !== 'audio/mpeg') {
