851cda8f642f964065da94cb105d102c491c36ba
Kirubel <se.kirubel.teka@gmail.com>
2024-05-13 00:04:20 +0300

    update comment list/ sort

--- src/views/Song.vue
+++ src/views/Song.vue
@@ -49,7 +49,7 @@
             </button>
           </vee-form>
           <!-- Sort Comments -->
-          <select
+          <select v-model="sort"
             class="block mt-4 py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
           >
             <option value="1">Latest</option>
@@ -60,7 +60,7 @@
     </section>
     <!-- Comments -->
     <ul class="container mx-auto">
-      <li class="p-6 bg-gray-50 border border-gray-200" v-for="comment in comments"
+      <li class="p-6 bg-gray-50 border border-gray-200" v-for="comment in sortedComments"
       :key="comment.docID">
         <!-- Comment Author -->
         <div class="mb-5">
@@ -93,10 +93,20 @@ export default {
             comment_alert_variant: 'bg-blue-500',
             comment_alert_message: 'Please wait! Submitting your comment.',
             comments: [],
+            sort: '1',
         };
     },
     computed: {
-        ...mapState(useUserStore,["userLoggedIn"])
+        ...mapState(useUserStore,["userLoggedIn"]),
+        sortedComments() {
+          return this.comments.slice().sort((a, b) => {
+            if (this.sort === '1') {
+              return new Date(b.datePosted) - new Date(a.datePosted);
+            }
+
+            return new Date(a.datePosted) - new Date(b.datePosted);
+          });
+        }
     },
     async created() {
         const docSnapshot = await songsCollection.doc(this.$route.params.id).get();
@@ -126,6 +136,8 @@ export default {
             };
 
             await commentsCollection.add(comment);
+
+            this.getComments();
 
             this.comment_in_submission = false;
             this.comment_alert_variant = 'bg-green-500';
