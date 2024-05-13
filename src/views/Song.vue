27c094e7bb25ea7bdf71a0556bb087869b17a1e6
Kirubel <se.kirubel.teka@gmail.com>
2024-05-14 00:31:53 +0300

    Query param for comment sort

--- src/views/Song.vue
+++ src/views/Song.vue
@@ -116,6 +116,10 @@ export default {
         return;
     }
 
+        const { sort } = this.$route.query;
+
+        this.sort = sort === '1' || sort === '2' ? sort : '1';
+
         this.song = docSnapshot.data();
         this.getComments();
     },
@@ -159,6 +163,19 @@ export default {
                 })
             })
         }
+    },
+    watch: {
+      sort(newVal) {
+        if (newVal === this.$route.query.sort) {
+          return;
+        }
+
+        this.$router.push({
+         query: {
+          sort: newVal,
+         }
+        })
+      }
     }
 }
 </script>
