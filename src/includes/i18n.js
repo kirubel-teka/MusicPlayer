9d8d85314cce14d72b4a12c25de3cc16c06e0899
Kirubel <se.kirubel.teka@gmail.com>
2024-05-30 22:52:24 +0300

    translate french setup

--- src/includes/i18n.js
+++ src/includes/i18n.js
@@ -1,10 +1,12 @@
-import { createi18n } from 'vue-i18n';
-import en from "@locales/en.json"
+import { createI18n } from 'vue-i18n';
+import en from "@/locales/en.json";
+import fr from "@/locales/fr.json";
 
-export default createi18n({
+export default createI18n({
     locale: "en",
     fallbackLocale: 'en',
     messages: {
         en,
+        fr,
     }
 })
