2d3e57fddee2a9e41f3c71a8fb4d45a9c0a531ab
Kirubel <se.kirubel.teka@gmail.com>
2024-06-01 11:55:05 +0300

    number localization

--- src/includes/i18n.js
+++ src/includes/i18n.js
@@ -8,5 +8,19 @@ export default createI18n({
     messages: {
         en,
         fr,
-    }
+    },
+    numberFormats: {
+        en: {
+            currency: {
+               style: "currency", 
+               currency: "USD",
+            },
+        },
+        ja: {
+            currency: {
+                style: "currency", 
+                currency: "JPY",
+             },
+        },
+    },
 })
