e15eaf138c455b9c0025c2ac98a71ac2cae3595c
Kirubel <se.kirubel.teka@gmail.com>
2024-06-03 00:51:21 +0300

    changing locals

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -27,6 +27,13 @@
           </template>
           
         </ul>
+        <ul class="ml-auto">
+          <li>
+            <a class="px-2 text-white" href="#" @click.prevent="changeLocale">
+              {{ currentLocale }} 
+            </a>
+          </li>
+        </ul>
       </div>
     </nav>
   </header>
@@ -41,6 +48,9 @@ export default {
   name: 'AppHeader',
   computed: {
     ...mapStores(useModalStore, useUserStore),
+    currentLocale() {
+      return this.$i18n.locale === "fr" ? "French" : "English";
+    }
   },
   methods: {
     toggleAuthModal() {
@@ -53,6 +63,9 @@ export default {
         this.$router.push({ name: 'home' })
       }
       
+    },
+    changeLocale() {
+      this.$i18n.locale = this.$i18n.locale === "fr" ? "en" : "fr";
     }
   }
 }
