2ec7d7da84274e860374d8588ef42bf4fd5f097b
Kirubel <se.kirubel.teka@gmail.com>
2024-11-27 13:22:47 +0300

    workbox

--- src/main.js
+++ src/main.js
@@ -9,8 +9,10 @@ import './assets/main.css'
 import VeeValidatePlugin from './includes/validation'
 import { auth } from "./includes/firebase"
 import Icon from './directives/icon'
-import i18n from './includes/i18n'
+import i18n from './includes/i18n';
+import { registerSW } from "virtual:pwa-register";
 
+registerSW({immediate: true});
 let app;
 auth.onAuthStateChanged(()=>{
     if (!app) {
--- vite.config.js
+++ vite.config.js
@@ -21,6 +21,9 @@ export default defineConfig({
           type: 'image/png',
         }
       ]
+    },
+    workbox: {
+      globPatterns: ["**/*.{js,css,html,png,jpg,}"],
     }
   })],
   resolve: {
