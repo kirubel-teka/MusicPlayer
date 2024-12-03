d09eec82a7dc564d8cc82a2b5ecdfd54c674f560
Kirubel <se.kirubel.teka@gmail.com>
2024-12-03 20:52:12 +0300

    sanity test

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,6 @@
+const { rule } = require('postcss')
+const { test, expect, vi, beforeEach, it } = require('vitest/dist/index.js')
+
 /* eslint-env node */
 require('@rushstack/eslint-patch/modern-module-resolution')
 
@@ -8,11 +11,24 @@ module.exports = {
     'eslint:recommended',
     '@vue/eslint-config-prettier/skip-formatting'
   ],
+
   overrides: [
     {
       files: ['cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}', 'cypress/support/**/*.{js,ts,jsx,tsx}'],
       extends: ['plugin:cypress/recommended']
-    }
+    },
+    {
+      files: ["src/components/__tests__/**.spec.js"],
+      globals: {
+        test: "readonly",
+        describe: "readonly",
+        expect: "readonly",
+        vi: "readonly",
+        beforeEach: "readonly",
+        it: "readonly",
+      },
+    },
+  
   ],
   env: {
     node: true
