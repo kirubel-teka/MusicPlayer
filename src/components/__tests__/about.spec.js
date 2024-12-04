c138df673c5dc87eefd1ca72cb15ef364bfda104
Kirubel <se.kirubel.teka@gmail.com>
2024-12-05 00:56:58 +0300

    test for about translations

--- src/components/__tests__/about.spec.js
+++ src/components/__tests__/about.spec.js
@@ -1,18 +1,41 @@
-import AboutView from "@/views/AboutView.vue";
-import { shallowMount } from "@vue/test-utils";
+import { shallowMount } from '@vue/test-utils';
+import AboutView from '@/views/AboutView.vue';
+import { createI18n } from 'vue-i18n';
+import en from '@/locales/en.json'; 
+import fr from '@/locales/fr.json'; 
 import { describe, test, expect } from "vitest";
 
+// Create an i18n instance with translations
+const i18n = createI18n({
+  locale: 'en', // Change this to 'fr' for French tests
+  messages: {
+    en,
+    fr,
+  },
+});
 
 describe('AboutView.vue', () => {
-    test('renders inner text', () => {
-        const wrapper = shallowMount(AboutView,{
-            global: {
-                mocks: {
-                    $t: (key) => key
-                }
-            }
-        });
+  test('renders translated text correctly in English', () => {
+    const wrapper = shallowMount(AboutView, {
+      global: {
+        plugins: [i18n],
+      },
+    });
 
-        expect(wrapper.text()).toContain('about');
-    })
-})
+    expect(wrapper.text()).toContain('users! If you have any questions,'); 
+  });
+
+
+  
+//   test('renders translated text correctly in French', async () => {
+//     await i18n.global.locale.change('fr');
+    
+//     const wrapper = mount(AboutView, {
+//       global: {
+//         plugins: [i18n],
+//       },
+//     });
+
+//     expect(wrapper.text()).toContain(''); 
+//   });
+});
